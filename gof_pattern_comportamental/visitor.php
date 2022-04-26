59c1ff5ee5980f11c2f222867a4510473d39bf3d
GeovaneF55 <geovanefss@gmail.com>
2022-04-26 15:03:58 +0100

    Padrão GOF - Estrutural (Visitor)

--- gof_pattern_comportamental/visitor.php
+++ gof_pattern_comportamental/visitor.php
@@ -1,3 +1,36 @@
 <div class="title">Visitor</div>
 
 <?php
+class Car
+{
+  public $color = "Silver";
+
+  // accepting Enhancer as visitor
+  public function visitor(Enhancer $enhancer)
+  {
+    $this->color = $enhancer->enhance($this);
+  }
+}
+
+// === A visitor ===
+// That can visit into Car object change it
+// The benefit is, Car object is now seperated from
+// detail algorithm of how it can be enhanced
+class Enhancer
+{
+  // visiting into Car
+  public function enhance(Car $car)
+  {
+    return "Matelic $car->color";
+  }
+}
+
+// ---
+
+$car = new Car();
+echo $car->color . "<br>";
+// Output: Silver
+
+$car->visitor(new Enhancer());
+echo $car->color . "<br>";
+// Output: Matelic Silver
