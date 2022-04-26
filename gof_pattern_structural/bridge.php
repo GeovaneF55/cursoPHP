62127700abcc607c807960aa1736b83bb5ecad96
GeovaneF55 <geovanefss@gmail.com>
2022-04-26 13:43:49 +0100

    Padrão GOF - Estrutural (Bridge, Proxy)

--- gof_pattern_structural/bridge.php
+++ gof_pattern_structural/bridge.php
@@ -1,4 +1,61 @@
 <div class="title">Bridge</div>
 
 <?php
+  interface Engine
+  {
+      public function run();
+  }
   
+  class Diesel implements Engine
+  {
+      public function run()
+      {
+          echo "Broom! Broom!<br>";
+      }
+  }
+  
+  class Petrol implements Engine
+  {
+      public function run()
+      {
+          echo "Wroom! Wroom!<br>";
+      }
+  }
+  
+  // === EngineBridge ===
+  // That help us switch engine type
+  // at run-time through set() method
+  class EngineBridge
+  {
+      public $engine;
+  
+      public function __construct(Engine $engine)
+      {
+          $this->engine = $engine;
+      }
+  
+      public function set(Engine $engine)
+      {
+          $this->engine = $engine;
+      }
+
+      public function run()
+      {
+          $this->engine->run();
+      }
+  }
+  
+  // ---
+  
+  $diesel = new Diesel();
+  $petrol = new Petrol();
+  
+  $bridge = new EngineBridge($diesel);
+  $bridge->run();
+  // Output:
+  // Broom! Broom!
+  
+  $bridge->set($petrol);
+  $bridge->run();
+  // Output:
+  // Wroom! Wroom!
--- gof_pattern_structural/proxy.php
+++ gof_pattern_structural/proxy.php
@@ -1,4 +1,43 @@
 <div class="title">Proxy</div>
 
 <?php
+  class Car
+  {
+    public function __construct($car)
+    {
+      echo "The $car has started and ready.\n";
+    }
+  }
   
+  // === CarProxy that forward to Car class ===
+  // We can have many Car objects but actually instantiated
+  // only when necessary with start() method (a.k.a lazy-load)
+  class CarProxy
+  {
+    private $car;
+
+    public function __construct($car)
+    {
+      $this->car = $car;
+    }
+
+    public function start()
+    {
+      new Car($this->car);
+    }
+  }
+  
+  // ---
+  
+  $cars = [
+    new CarProxy("Fit"),
+    new CarProxy("Vitz"),
+    new CarProxy("Civic"),
+    new CarProxy("Polo"),
+  ];
+  $cars[1]->start();
+  // Output: The Vitz has started and ready
+  echo "<br>";
+
+  $cars[3]->start();
+  // Output: The Polo has started and ready.
