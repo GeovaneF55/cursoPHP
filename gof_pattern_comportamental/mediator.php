ed3d82e8b57b34ed412961907cb90cdd97695861
GeovaneF55 <geovanefss@gmail.com>
2022-04-26 14:38:03 +0100

    Padrão GOF - Estrutural (Mediator)

--- gof_pattern_comportamental/mediator.php
+++ gof_pattern_comportamental/mediator.php
@@ -1,3 +1,54 @@
 <div class="title">Mediator</div>
 
 <?php
+interface Filter
+{
+    public function check();
+}
+
+// === A Colleague ===
+class OilFilter implements Filter
+{
+     public function check()
+     {
+         echo "Checking Oil Filter... OK<br>";
+     }
+}
+
+// === A Colleague ===
+class AirFilter implements Filter
+{
+     public function check()
+     {
+         echo "Checking Air Filter... OK<br>";
+     }
+}
+
+// === A Mediator ===
+// Collection of colleagues designed to make
+// all those colleague work together
+class Car
+{
+    protected $filters;
+
+    public function addFilter(Filter $filter)
+    {
+        $this->filters[] = $filter;
+    }
+
+    public function check()
+    {
+        foreach ($this->filters as $filter) {
+            $filter->check();
+        }
+    }
+}
+
+$car = new Car();
+$car->addFilter(new OilFilter());
+$car->addFilter(new AirFilter());
+$car->check();
+
+// Output:
+// Checking Oil Filter... OK
+// Checking Air Filter... OK
